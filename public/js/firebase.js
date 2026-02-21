c0984aed74a8b41298d81bc730a9cd70d4e2f00b
PedroMaia21 <pedroabmaia@hotmail.com>
2026-02-21 23:00:41 +0000

    Security change

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -3,14 +3,13 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRs03-iAhseSiLl5Q20SMCVtEhcega52E",
-  authDomain: "pedromaia-os.firebaseapp.com",
-  projectId: "pedromaia-os",
-  storageBucket: "pedromaia-os.firebasestorage.app",
-  messagingSenderId: "794714317669",
-  appId: "1:794714317669:web:26743812b2fbadee77f78c"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
