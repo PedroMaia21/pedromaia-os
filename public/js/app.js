c29a33a60d74e14a09c6ed7cc0e8b417d5e806aa
PedroMaia21 <pedroabmaia@hotmail.com>
2026-03-08 15:24:37 +0000

    #1 added home button and modified router logic

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
-import { loadModule } from "./router.js";
+import { initRouter } from "./router.js";
 import { getCurrentUser } from "./auth.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+
     const user = await getCurrentUser();
+
     if (!user) {
         window.location.href = "/login.html";
         return;
@@ -13,9 +15,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const appContainer = document.getElementById("app");
     if (appContainer) appContainer.style.display = "grid";
 
-    const btnSideQuests = document.getElementById("btn-side-quests");
-    if (btnSideQuests) btnSideQuests.addEventListener("click", () => loadModule("sideQuests"));
+    initRouter();
 
-    const btnPlaylists = document.getElementById("btn-playlists");
-    if (btnPlaylists) btnPlaylists.addEventListener("click", () => loadModule("playlists"));
 });
--- public/js/router.js
+++ public/js/router.js
@@ -1,19 +1,43 @@
+let homeHTML = "";
+
+export function initRouter() {
+
+    const content = document.getElementById("content");
+    homeHTML = content.innerHTML;
+
+    document.addEventListener("click", (e) => {
+
+        const route = e.target.dataset.route;
+        if (!route) return;
+
+        if (route === "home") {
+            loadHome();
+        } else {
+            loadModule(route);
+        }
+
+    });
+}
+
+export function loadHome() {
+    document.getElementById("content").innerHTML = homeHTML;
+}
+
 export async function loadModule(name) {
+
     const path = `/modules/${name}/${name}.html`;
-    
+
     const res = await fetch(path);
-    
-    if(!res.ok) {
-        throw new Error("Failed to load ${path}");
+
+    if (!res.ok) {
+        throw new Error(`Failed to load ${path}`);
     }
 
     const html = await res.text();
 
     document.getElementById("content").innerHTML = html;
 
+    const module = await import(`/modules/${name}/${name}.js`);
 
-    const module = await import(
-        `/modules/${name}/${name}.js`
-    );
-    module.init();
+    if (module.init) module.init();
 }
