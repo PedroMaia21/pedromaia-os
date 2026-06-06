dab7795aaf2c05c629576ef1106d4188d8a71b3b
PedroMaia21 <pedroabmaia@hotmail.com>
2026-06-06 16:34:30 +0100

    started side quest module: added enums for categories and status

---
+++ public/modules/sideQuests/sideQuests.seed.js
@@ -0,0 +1,14 @@
+export const SideQuestCategory = {
+    DECOMPRESSION: "DECOMPRESSION",
+    MEDIA: "MEDIA",
+    SKILL: "SKILL",
+    LIFE: "LIFE"
+}
+
+export const SideQuestStatus = {
+    AVAILABLE: "AVAILABLE",
+    ACTIVE: "ACTIVE",
+    COMPLETED: "COMPLETED",
+    EXPIRED: "EXPIRED",
+    SKIPPED: "SKIPPED"
+}
